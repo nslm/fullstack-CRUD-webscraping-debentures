c6d5f5dae022192e519f534a9858ca46b4dff6a0
nslm <anselmo.clemente8@gmail.com>
2025-08-06 00:42:43 -0300

    adicinando configs de docker e rotas do backend

--- backend/app/routes/__init__.py
+++ backend/app/routes/__init__.py
@@ -1,8 +1,8 @@
-from analytics.caracteristicas import router as analytics_caracteristicas
-from analytics.evolucao import router as analytics_evolucao
-from coleta.balcao import router as coleta_balcao
-from coleta.caracteristicas import router as coleta_caracteristicas
-from debentures.debentures import router as debentures
+from .analytics.caracteristicas import router as analytics_caracteristicas
+from .analytics.evolucao import router as analytics_evolucao
+from .coleta.balcao import router as coleta_balcao
+from .coleta.caracteristicas import router as coleta_caracteristicas
+from .debentures.debentures import router as debentures
 
 
 __all__ = ["analytics_caracteristicas", "analytics_evolucao", "coleta_balcao", "coleta_caracteristicas", "debentures"]
